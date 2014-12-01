1b38568f79487b1480c15e71dfe21704874b22d2
WinMac32 <seth@viaware.ca>
2014-12-01 16:12:16 -0700

    Update license and readme

--- src/main/java/ca/viaware/tileset/gui/editor/panel/EditorPanel.java
+++ src/main/java/ca/viaware/tileset/gui/editor/panel/EditorPanel.java
@@ -1,14 +1,14 @@
 /*
 Copyright 2014 Seth Traverse
 
-This file is part of Project Sierra.
+This file is part of SwankyTS.
 
-Project Sierra is free software: you can redistribute it and/or modify
+SwankyTS is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Project Sierra is distributed in the hope that it will be useful,
+SwankyTS is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
